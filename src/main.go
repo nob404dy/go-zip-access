c38de6e583c94ddc5ff5383fc552d6763becd99f
nob404dy <nob404dy@protonmail.com>
2021-05-21 01:28:01 +0200

    file listing

--- src/main.go
+++ src/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
-
+	"encoding/binary"
 )
 
 // Reading files requires checking most calls for errors.
@@ -15,30 +15,82 @@ func check(e error) {
 	}
 }
 
-func search_signature(f *os.File, file_length int64, byte_size int, x []byte) int64 {
+
+func search_signature(f *os.File, file_length int64, start_offset int64, byte_size int, target []byte) int64 {
 
 	for i := 1; i < int(file_length); i++ {
 
-		// scan every byte_block, starting from the end
-		f.Seek(file_length - int64(byte_size + i), 0)
+		// scan every byte_block, starting from the end with start_offset
+		f.Seek(file_length - start_offset - int64(byte_size + i ), 0)
 
 		b := make([]byte, byte_size)
 
-		n2, err := io.ReadAtLeast(f, b, byte_size)
+		n, err := io.ReadAtLeast(f, b, byte_size)
 		check(err)
 
-		if string(b[:n2]) == string(x) {
+		if string(b[:n]) == string(target) {
 			index := file_length - int64(i)
 			return index
 			break
 		}
 	}
 
-	panic("signature" + string(x) + " not found")
+	panic("signature not found")
+}
+
+func read_bytes(f *os.File, amount int, location int64) []byte{
+	f.Seek(location, 0)
+	b := make([]byte, amount)
+	n, err := io.ReadAtLeast(f, b, amount)
+	check(err)
+	return b[:n]
+}
+
+func list_files(f *os.File, file_length int64, start_offset int64, target []byte, interrupt []byte) []int64{
+
+	var list []int64
+
+	for i := 1; i < int(file_length); i++ {
+
+		// scan every byte_block, starting from the end with start_offset
+		test_location := file_length - start_offset - int64(4 + i )
+		test_slice := read_bytes(f, 4, test_location)
+
+		if string(test_slice) == string(target) {
+			index := file_length - int64(i) - 4
+			Central_FH_slice := read_bytes(f, 4, index)
+			fmt.Print("found Central directory file header")
+			fmt.Println(Central_FH_slice)
+
+			version_slice := read_bytes(f, 2, index+4)
+			fmt.Print("Version ")
+			fmt.Println(version_slice)
+
+			name_len_slice := read_bytes(f, 2, index+28)
+			name_len := int(binary.LittleEndian.Uint16(name_len_slice))
+			fmt.Print("Name Length: ")
+			fmt.Print(name_len_slice)
+			fmt.Print(" -> ")
+			fmt.Println(name_len)
+
+			name := read_bytes(f, name_len, index+46)
+			fmt.Print("Name: ")
+			fmt.Println(string(name))
+
+			list = append(list,index)
+		}
+		if string(test_slice) == string(interrupt){
+			fmt.Println("found interrupt")
+			return list
+			break
+		}
+	}
+
+	panic("signature not found")
 }
 
 func main() {
-	zip_path := "library_tiny.zip"
+	zip_path := "library.zip"
 	fmt.Println("opening " + zip_path)
 
 	// read file
@@ -54,15 +106,20 @@ func main() {
 	fmt.Println(" Bytes")
 
 	// End of central directory signature =  0x06054b50
-	EoCD := search_signature(f, file_length, 4, []byte{0x50,0x4b,0x05,0x06})
+	EoCD := search_signature(f, file_length, 0, 4, []byte{0x50,0x4b,0x05,0x06})
 	fmt.Print(EoCD)
 	fmt.Println(" - End of central directory")
 
-	// Central directory file header signature = 0x02014b50
-	CDFH := search_signature(f, file_length, 4, []byte{0x50,0x4b,0x01,0x02})
-	fmt.Print(CDFH)
-	fmt.Println(" - Central directory file header ")
 
+
+	// Local file header signature = 0x04034b50
+	//Local_FH := search_signature(f, file_length, 0, 4, []byte{0x50,0x4b,0x03,0x04})
+	//fmt.Print(Local_FH)
+	//fmt.Println(" - Local file header ")
+
+
+	// interrupts at the first local file header 0x04034b50
+	fmt.Print(list_files(f, file_length, 0, []byte{0x50,0x4b,0x01,0x02}, []byte{0x50,0x4b,0x03,0x04}))
 
 	f.Close()
 }
